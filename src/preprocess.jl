94afa6ce63ad12e0f0ad7bdfe68c078f549f4361
ymtoo <ymtoo86@gmail.com>
2020-11-18 16:03:47 +0800

    fix(preprocess): use CauchyNL

--- src/preprocess.jl
+++ src/preprocess.jl
@@ -4,11 +4,10 @@ function removedc_whiten_lpf_resample(x::AbstractVector{T},
                                       rate=1//10) where {T<:Real}
     x = removedc(x .- median(x))
     x = whiten(x, 8192, 0)
+    σ = mad(x)
+    x = MemorylessNonlinearities.filt(CauchyNL(1 * σ), x)
 #    d = AlphaStableDistributions.fit(SymmetricAlphaStable, x)
 #    x = MemorylessNonlinearities.filt(SαS(d.α, d.scale, d.location), x)
-    lpf = digitalfilter(Lowpass(4800; fs=fs), FIRWindow(hanning(127)))
-    x = resample(filtfilt(lpf, x), 1//10)
-    σ = mad(x)
-    x ./= σ
-    x = MemorylessNonlinearities.filt(Arctangent(1), x)
+    bpf = digitalfilter(Bandpass(10, lpfreq; fs=fs), FIRWindow(hanning(8191)))
+    resample(filtfilt(bpf, x), rate)
 end
