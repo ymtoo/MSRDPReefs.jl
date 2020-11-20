e6e12f4f55f4cb1fcc2d4df5991852aaf17627f0
ymtoo <ymtoo86@gmail.com>
2020-11-20 12:10:45 +0800

    fix(preprocess): use a large scale

--- src/preprocess.jl
+++ src/preprocess.jl
@@ -5,7 +5,7 @@ function removedc_whiten_lpf_resample(x::AbstractVector{T},
     x = removedc(x .- median(x))
     x = whiten(x, 8192, 0)
     σ = mad(x)
-    x = MemorylessNonlinearities.filt(CauchyNL(1 * σ), x)
+    x = MemorylessNonlinearities.filt(CauchyNL(3 * σ), x)
 #    d = AlphaStableDistributions.fit(SymmetricAlphaStable, x)
 #    x = MemorylessNonlinearities.filt(SαS(d.α, d.scale, d.location), x)
     bpf = digitalfilter(Bandpass(10, lpfreq; fs=fs), FIRWindow(hanning(16383)))
