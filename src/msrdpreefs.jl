b67145057ba8051b6b907a85917cd765c65efe4c
ymtoo <ymtoo86@gmail.com>
2020-12-10 19:01:56 +0800

    docs(msrdpreefs): add docstrings

--- src/msrdpreefs.jl
+++ src/msrdpreefs.jl
@@ -1,8 +1,3 @@
-"""
-Get parent paths. The output is a 1-D array conataining paths for data (acoustic and environmental) collected in all the sites.
-"""
-getpaths(rootpath::AbstractString) = [joinpath(rootpath, dir) for dir in readdir(rootpath) if dir ∉ ["Semakau-NE", "README.md"]]
-
 const DEFAULTDTRANGES = (DateTime(2019, 6, 1, 0, 0, 0), DateTime(2021, 4, 30, 23, 59, 59))
 const SITEDTRANGES = Dict(
     "Hantu-W" => [(DateTime(2019, 9, 11, 13, 0, 0), DateTime(2019, 10, 11, 12, 0, 0)), 
@@ -61,9 +56,14 @@ const COLUMNTYPES = [DateTime[],
 const WAVFILETIMELENGTH = Minute(5)
 
 """
-Metadata describing the collected data at a particular site. The output is a Metadata 
-(a new type) containing a root path of the data, a site name, a datatime range and a metadata DataFrame.
+    getpaths(rootpath)
+
+Get parent paths. The output is a 1-D array containing paths for data 
+(acoustic and environmental) collected in all the sites.
 """
+getpaths(rootpath::AbstractString) = [joinpath(rootpath, dir) for dir in 
+    readdir(rootpath) if dir ∉ ["Semakau-NE", "README.md"]]
+
 struct Metadata
     path::AbstractString
     site::AbstractString
@@ -71,6 +71,42 @@ struct Metadata
     df::AbstractDataFrame
 end
 
+"""
+    Metadata(path, dtranges=SITEDTRANGES[split(path, "/")[end]])
+
+Get Metadata describing the collected data at a particular site. It composes 
+of a root path `path` to the data, a site name `site`, datatime ranges `dtranges` 
+and a DataFrame `df` for metadata: 
+- datetime
+- sensitivity
+- gain
+- wavpath
+- battery
+- depth
+- temperature
+- red light
+- blue light
+- green light 
+
+# Example:
+```julia-repl
+julia> Metadata("/home/arl/Data/reefwatch/recordings/Hantu-W")
+Metadata of Hantu-W during [(Dates.DateTime("2019-09-11T13:00:00"),
+Dates.DateTime("2019-10-11T12:00:00")), (Dates.DateTime("2020-01-08T12:30:00"),
+Dates.DateTime("2020-02-16T00:11:31")), (Dates.DateTime("2020-06-30T12:30:00"),
+Dates.DateTime("2020-07-30T21:00:00")), (Dates.DateTime("2020-10-13T11:00:00"),
+Dates.DateTime("2020-11-13T10:00:00"))]
+5×11 DataFrame
+│ Row │ datetime            │ sensitivity │ gain    │ wavpath                                                                         │ battery  │ depth    │ temperature │ redlight │ greenlight │ bluelight │ moonphase │
+│     │ Dates.DateTime      │ Float64     │ Float64 │ String                                                                          │ Float64? │ Float64? │ Float64?    │ Float64? │ Float64?   │ Float64?  │ Int64     │
+├─────┼─────────────────────┼─────────────┼─────────┼─────────────────────────────────────────────────────────────────────────────────┼──────────┼──────────┼─────────────┼──────────┼────────────┼───────────┼───────────┤
+│ 1   │ 2019-09-11T13:02:08 │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T130208.wav │ 4.31     │ 2.61     │ 30.3        │ 6303.0   │ 10794.0    │ 6907.0    │ 5         │
+│ 2   │ 2019-09-11T13:07:07 │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T130707.wav │ 4.3      │ 2.6      │ 30.28       │ 6911.0   │ 10755.0    │ 7262.0    │ 5         │
+│ 3   │ 2019-09-11T13:12:07 │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T131207.wav │ 4.3      │ 2.56     │ 30.21       │ 6407.0   │ 10623.0    │ 6929.0    │ 5         │
+│ 4   │ 2019-09-11T13:17:07 │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T131707.wav │ 4.28     │ 2.54     │ 30.22       │ 6008.0   │ 10385.0    │ 6814.0    │ 5         │
+│ 5   │ 2019-09-11T13:22:07 │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T132207.wav │ 4.28     │ 2.51     │ 30.23       │ 6227.0   │ 10614.0    │ 6899.0    │ 5         │
+```
+"""
 function Metadata(path::AbstractString, dtranges::AbstractVector{Tuple{DateTime, DateTime}}=SITEDTRANGES[split(path, "/")[end]])
     site = split(path, "/")[end]
     logpaths = getlogpaths(path, dtranges)
@@ -94,6 +130,16 @@ function Metadata(path::AbstractString, index::Integer)
     Metadata(path, SITEDTRANGES[split(path, "/")[end]][index:index])
 end
 
+function Base.show(io::IO, ::MIME"text/plain", x::Metadata) 
+    println(io, "Metadata of $(x.site) during $(x.dtranges)")
+    println(io, first(x.df, 5))
+end
+
+"""
+    getlogpaths(path, dtranges)
+
+Get paths of all the log files in parent path `path`
+"""
 function getlogpaths(path::AbstractString, dtranges::AbstractVector{Tuple{DateTime, DateTime}})
     logpaths = Vector{String}()
     dirs = [dir for dir in readdir(path) if dir[1] ∉ ['.', '@']]
@@ -109,9 +155,11 @@ function getlogpaths(path::AbstractString, dtranges::AbstractVector{Tuple{DateTi
 end
 
 """
-Add DateTime and WAV path columns to the DataFrame. Combine Red, 
-Green and Blue into RGB(Red, Green, Blue). Rename the columns. Remove 
-rows which are not in the datetime range.
+    revisedf!(df, logpath, dtrange, acousticsensor)    
+
+Modify the dataframe `df` by adding DateTime and WAV path columns, 
+combining Red, Green and Blue into RGB(Red, Green, Blue), renaming 
+the columns and removing rows which are not in the datetime range.
 """
 function revisedf!(df, logpath, dtrange, acousticsensor)
 #    colnames = names(df)#["Datetime", "WAV path", "Batt (V)", "Depth (m)", "Temp (degC)", "Light Intensity"]
@@ -179,15 +227,64 @@ function MetadataAll(paths::Vector{<:AbstractString},
     end
     MetadataAll(paths, dtrangesdict, df)
 end
+"""
+    MetadataAll(path, dtrangesdict=SITEDTRANGES)
+
+Get MetadataAll describing the collected data at all the sites. It composes 
+of a root path `path` to the data, datatime ranges `dtrangesdict` 
+and a DataFrame `df` for metadata: 
+- datetime
+- sensitivity
+- gain
+- wavpath
+- battery
+- depth
+- temperature
+- red light
+- blue light
+- green light 
+
+# Example:
+```julia-repl
+julia> MetadataAll("/home/arl/Data/reefwatch/recordings")
+Metadata of all the sites
+5×12 DataFrame
+│ Row │ datetime            │ site     │ sensitivity │ gain    │ wavpath                                                                         │ battery  │ depth    │ temperature │ redlight │ greenlight │ bluelight │ moonphase │
+│     │ Dates.DateTime      │ SubStri… │ Float64     │ Float64 │ String                                                                          │ Float64? │ Float64? │ Float64?    │ Float64? │ Float64?   │ Float64?  │ Int64     │
+├─────┼─────────────────────┼──────────┼─────────────┼─────────┼─────────────────────────────────────────────────────────────────────────────────┼──────────┼──────────┼─────────────┼──────────┼────────────┼───────────┼───────────┤
+│ 1   │ 2019-09-11T13:02:08 │ Hantu-W  │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T130208.wav │ 4.31     │ 2.61     │ 30.3        │ 6303.0   │ 10794.0    │ 6907.0    │ 5         │
+│ 2   │ 2019-09-11T13:07:07 │ Hantu-W  │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T130707.wav │ 4.3      │ 2.6      │ 30.28       │ 6911.0   │ 10755.0    │ 7262.0    │ 5         │
+│ 3   │ 2019-09-11T13:12:07 │ Hantu-W  │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T131207.wav │ 4.3      │ 2.56     │ 30.21       │ 6407.0   │ 10623.0    │ 6929.0    │ 5         │
+│ 4   │ 2019-09-11T13:17:07 │ Hantu-W  │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T131707.wav │ 4.28     │ 2.54     │ 30.22       │ 6008.0   │ 10385.0    │ 6814.0    │ 5         │
+│ 5   │ 2019-09-11T13:22:07 │ Hantu-W  │ -179.8      │ 16.77   │ /home/arl/Data/reefwatch/recordings/Hantu-W/2019-09/2019-09/20190911T132207.wav │ 4.28     │ 2.51     │ 30.23       │ 6227.0   │ 10614.0    │ 6899.0    │ 5         │
+```
+"""
+function MetadataAll(path::AbstractString, dtrangesdict::Dict{String, Vector{Tuple{DateTime, DateTime}}}=SITEDTRANGES)
+    MetadataAll(getpaths(path), dtrangesdict)
+end
+
+function Base.show(io::IO, ::MIME"text/plain", x::MetadataAll) 
+    println(io, "Metadata of all the sites")
+    println(first(x.df, 5))
+end
+
 
 """
-Show the data collection progress in percentage. 100 percent equals 
-to one month of data.
+    datacollectionprogress(paths)
+
+Show the data collection progress in percentage given multipe parent paths. 
+100 percent equals to one month of data.
 
 # Examples:
 ```julia-repl
-julia> MSRDPReefs.Msrdpreefs.datacollectionprogress("/mnt/arl-nas-01/
-msrdp-reef-monitoring/recordings")
+julia> datacollectionprogress(["/home/arl/Data/reefwatch/recordings/Hantu-W",
+"/home/arl/Data/reefwatch/recordings/RL-W"])
+2×7 DataFrame
+│ Row │ Site    │ D1 (%)  │ D2 (%)  │ D3 (%)  │ D4 (%)  │ D5 (%)  │ D6 (%)  │
+│     │ String  │ Real?   │ Real?   │ Real?   │ Real?   │ Real?   │ Real?   │
+├─────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
+│ 1   │ Hantu-W │ 100.788 │ 128.408 │ 77.8376 │ 103.189 │ missing │ missing │
+│ 2   │ RL-W    │ 83.6276 │ 128.501 │ 116.384 │ missing │ missing │ missing │
 ```
 """
 function datacollectionprogress(paths::AbstractVector{String})
@@ -216,35 +313,60 @@ function datacollectionprogress(paths::AbstractVector{String})
     end
     df
 end
+"""
+    datacollectionprogress(path)
+
+Show the data collection progress in percentage given a root path. 100 percent
+equals to one month of data.
+
+# Examples:
+```julia-repl
+julia> datacollectionprogress("/home/arl/Data/reefwatch/recordings")
+10×7 DataFrame
+│ Row │ Site       │ D1 (%)  │ D2 (%)  │ D3 (%)  │ D4 (%)  │ D5 (%)  │ D6 (%)  │
+│     │ String     │ Real?   │ Real?   │ Real?   │ Real?   │ Real?   │ Real?   │
+├─────┼────────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
+│ 1   │ Hantu-W    │ 100.788 │ 128.408 │ 77.8376 │ 103.189 │ missing │ missing │
+│ 2   │ Jong-S     │ 100.811 │ 116.446 │ 103.188 │ missing │ missing │ missing │
+│ 3   │ Kusu-NE    │ 100.452 │ 116.518 │ missing │ missing │ missing │ missing │
+│ 4   │ RL-W       │ 83.6276 │ 128.501 │ 116.384 │ missing │ missing │ missing │
+│ 5   │ SD-NW      │ 33.1604 │ 44.6388 │ 103.184 │ missing │ missing │ missing │
+│ 6   │ SL-SE      │ 44.5767 │ 103.188 │ missing │ missing │ missing │ missing │
+│ 7   │ Semakau-NW │ 128.225 │ 116.374 │ missing │ missing │ missing │ missing │
+│ 8   │ Semakau-SW │ 18.9137 │ 4.374   │ missing │ missing │ missing │ missing │
+│ 9   │ Seringat   │ 90.5111 │ 51.5231 │ 106.451 │ missing │ missing │ missing │
+│ 10  │ TPT        │ 128.354 │ 25.6559 │ 106.307 │ missing │ missing │ missing │
+```
+"""
 function datacollectionprogress(root::AbstractString)
     paths = [joinpath(root, p) for p in readdir(root) if isdir(joinpath(root), p) && p ∉ ["Semakau-NE", "README.md"]]
     datacollectionprogress(paths)
 end
 
-"""
-Filter extreme feature values.
-"""
-function filterfeatures!(df::DataFrame, feas::Vector{Symbol})
-    numrows = size(df, 1)
-    for fea in feas
-        x = df[!, fea]
-        if fea == :nₛ
-            low = percentile(x, 1)
-            indices = findall(x -> x<low, x)
-        elseif fea == :μₜ
-            high = percentile(x, 99)
-            indices = findall(x -> x>high, x)
-        elseif fea == :varₜ
-            high = percentile(x, 99)
-            indices = findall(x -> x>high, x)
-        elseif fea == :α
-            high = percentile(x, 99)
-            indices = findall(x -> x>high, x)
-        elseif fea == :scale
-            low, high = percentile(x, [1, 99])
-            indices = findall(x -> (x<low) || (x>high), x)
-        end
-        df[indices, fea] .= median(x)
-    end
-    df
-end
+# """
+# Filter extreme feature values.
+# """
+# function filterfeatures!(df::DataFrame, feas::Vector{Symbol})
+#     numrows = size(df, 1)
+#     for fea in feas
+#         x = df[!, fea]
+#         if fea == :nₛ
+#             low = percentile(x, 1)
+#             indices = findall(x -> x<low, x)
+#         elseif fea == :μₜ
+#             high = percentile(x, 99)
+#             indices = findall(x -> x>high, x)
+#         elseif fea == :varₜ
+#             high = percentile(x, 99)
+#             indices = findall(x -> x>high, x)
+#         elseif fea == :α
+#             high = percentile(x, 99)
+#             indices = findall(x -> x>high, x)
+#         elseif fea == :scale
+#             low, high = percentile(x, [1, 99])
+#             indices = findall(x -> (x<low) || (x>high), x)
+#         end
+#         df[indices, fea] .= median(x)
+#     end
+#     df
+# end
